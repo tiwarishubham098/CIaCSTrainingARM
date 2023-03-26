8352ca49e94c38b5888532e3ceb8ea784110d6bb
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2023-03-27 00:27:29 +0530

    Create installTools.sh

---
+++ installTools.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install -y wget apt-transport-https software-properties-common
+wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update
+sudo apt-get install -y powershell
+pwsh
